46ded712a6bb7631ffb0f2ce30c0eedc72e2bbe0
Aidan <walkeraidan94@gmail.com>
2021-03-31 16:10:21 +0100

    Updates

---
+++ precache-manifest.f12070941b51e2b2109835701ffd7a68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e94f5cf215092d22abafbece04b0c11",
+    "url": "/css_grid-real_estate_site/index.html"
+  },
+  {
+    "revision": "2bde155230dba5778778",
+    "url": "/css_grid-real_estate_site/static/css/main.b9f221cd.chunk.css"
+  },
+  {
+    "revision": "3b4409df8b1fb5c0a4d4",
+    "url": "/css_grid-real_estate_site/static/js/2.7d471ecf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/css_grid-real_estate_site/static/js/2.7d471ecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bde155230dba5778778",
+    "url": "/css_grid-real_estate_site/static/js/main.8cd030e4.chunk.js"
+  },
+  {
+    "revision": "636876a37524c8cac623",
+    "url": "/css_grid-real_estate_site/static/js/runtime-main.bd2a425b.js"
+  },
+  {
+    "revision": "f250e12628a7b2a12cb9141fa2e5cda1",
+    "url": "/css_grid-real_estate_site/static/media/back.f250e126.jpg"
+  },
+  {
+    "revision": "d11b666fa6c694c620ff867c6669cf7f",
+    "url": "/css_grid-real_estate_site/static/media/hero.d11b666f.jpeg"
+  }
+]);
